80be163550739f0c01c023a83c0376597f0ec27c
shsobur <shsoburhossen951@gmail.com>
2025-04-05 23:54:53 +0600

    Change server url

--- src/Hook/useAxios.jsx
+++ src/Hook/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://communion-hub-server-theta.vercel.app",
 });
 
 const useAxios = () => {
