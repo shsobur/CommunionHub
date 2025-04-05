f6b92e5174fe3ea9f4f519471be57ddb57586435
shsobur <shsoburhossen951@gmail.com>
2025-04-05 22:59:48 +0600

    Event page event display completed

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -1,13 +1,47 @@
 import "../Style/EventCaed.css";
+import { GoArrowRight } from "react-icons/go";
+import { CiCalendarDate, CiLocationOn } from "react-icons/ci";
 
-const EventCard = () => {
+const EventCard = ({ event }) => {
+  const { image, title, date, location, description } = event;
+
   return (
     <>
-      <section>
-        Hello this is event card container
+      <section className="event_card_top_container">
+        <div>
+          <img src={image} alt="Image" />
+        </div>
+
+        <div className="event_card_title_container">
+          <h2>{title.length < 10 
+            ? `${title}` : `${title.slice(0, 20)}...`
+          }</h2>
+          <h3>
+            {description.lenght < 50
+              ? `${description}`
+              : `${description.slice(0, 50)}...`}
+          </h3>
+        </div>
+
+        <div className="event_card_post_info_container">
+          <ul>
+            <li>
+              <CiCalendarDate />
+              {date}
+            </li>
+            <li>
+              <CiLocationOn />
+              {location}
+            </li>
+          </ul>
+        </div>
+
+        <button>
+          Read More <GoArrowRight />
+        </button>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
--- src/Page/EventPage/Event.jsx
+++ src/Page/EventPage/Event.jsx
@@ -1,14 +1,42 @@
-import { useState } from "react";
 import "../../Style/Event.css";
+import useAxios from "../../Hook/useAxios";
+import { useEffect, useState } from "react";
 import EventCard from "../../Components/EventCard";
+import Loading from "../../Components/Loading";
 
 const Event = () => {
+  const axiosPublic = useAxios();
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(events);
   const [activeCategory, setActiveCategory] = useState("All");
   const categories = ["All", "Religious", "Social", "Charity"];
+
+  useEffect(() => {
+    axiosPublic.get("/events").then((res) => {
+      setLoading(true);
+      setEvents(res.data);
+      setLoading(false);
+    });
+  }, [axiosPublic]);
 
   const handleCategory = (e) => {
     const categories = e.target.value;
     setActiveCategory(categories);
+
+    if(categories === "All") {
+      axiosPublic.get("/events").then((res) => {
+        setLoading(true);
+        setEvents(res.data);
+        setLoading(false);
+      });
+    }
+
+    axiosPublic.get(`/event?category=${categories}`).then((res) => {
+      setLoading(true);
+      setEvents(res.data);
+      setLoading(false);
+    });
   };
 
   return (
@@ -34,7 +62,12 @@ const Event = () => {
           </div>
         </div>
 
-        <div className="main_event_catd_container"><EventCard></EventCard></div>
+        <div className="main_event_card_container">
+          {
+            loading ? <div className="w-full text-2xl text-center font-bold mt-10 max-w-[1536px] m-auto ">Loading...</div> :
+            events.map(event => <EventCard key={event._id} event={event}></EventCard>)
+          }
+        </div>
       </section>
     </>
   );
